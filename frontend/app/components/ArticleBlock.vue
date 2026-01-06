4177be48f9b9055b40ef2f876f346bb4160cb400
Jack Barham <>
2026-01-06 19:31:33 +0000

    Move PortableTextImage into a separate component

--- frontend/app/components/ArticleBlock.vue
+++ frontend/app/components/ArticleBlock.vue
@@ -9,54 +9,13 @@
 <script setup lang="ts">
 import { PortableText } from '@portabletext/vue'
 import type { PortableTextBlock } from '@portabletext/types'
-
-interface SanityImageAsset {
-  _ref: string
-  _type: string
-}
-
-interface SanityImage {
-  _type: string
-  asset: SanityImageAsset
-  alt?: string
-  caption?: string
-}
+import PortableTextImage from './portabletext/PortableTextImage.vue'
 
 defineProps<{
   _key: string
   _type: string
   content?: PortableTextBlock[]
 }>()
-
-const PortableTextImage = defineComponent({
-  props: {
-    value: {
-      type: Object as PropType<SanityImage>,
-      required: true,
-    },
-  },
-  setup(props) {
-    const imageId = computed(() => props.value?.asset?._ref || '')
-
-    return () =>
-      imageId.value
-        ? h('figure', { class: 'portable-text-image' }, [
-            h(resolveComponent('NuxtImg'), {
-              provider: 'sanity',
-              src: imageId.value,
-              alt: props.value?.alt || '',
-              sizes: '100vw sm:100vw md:780px',
-              loading: 'lazy',
-              class: 'rounded-lg',
-            }),
-            props.value?.caption
-              ? h('figcaption', props.value.caption)
-              : null,
-          ])
-        : null
-  },
-})
-
 
 const portableTextComponents: any = {
   types: {
---
+++ frontend/app/components/portabletext/PortableTextImage.vue
@@ -0,0 +1,33 @@
+<template>
+  <figure v-if="imageId" class="portable-text-image">
+    <NuxtImg
+      provider="sanity"
+      :src="imageId"
+      :alt="value?.alt || ''"
+      sizes="100vw sm:100vw md:780px"
+      loading="lazy"
+      class="rounded-lg"
+    />
+    <figcaption v-if="value?.caption" class="text-center">{{ value.caption }}</figcaption>
+  </figure>
+</template>
+
+<script setup lang="ts">
+interface SanityImageAsset {
+  _ref: string
+  _type: string
+}
+
+interface SanityImage {
+  _type: string
+  asset: SanityImageAsset
+  alt?: string
+  caption?: string
+}
+
+const props = defineProps<{
+  value: SanityImage
+}>()
+
+const imageId = computed(() => props.value?.asset?._ref || '')
+</script>
