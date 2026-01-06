b207e651a33d757a9014e5928d9001e6200fa330
Jack Barham <>
2026-01-06 13:20:28 +0000

    Code formatting

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,8 +1,8 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
-import {structure} from './structure'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import { structure } from './structure'
 
 export default defineConfig({
   name: 'default',
