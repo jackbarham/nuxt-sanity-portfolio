35a7094ba22f5788869514f1b96b2e8dcebad7d5
Jack Barham <>
2026-01-10 10:23:40 +0000

    Restyle footer

--- frontend/app/components/MainFooter.vue
+++ frontend/app/components/MainFooter.vue
@@ -1,24 +1,22 @@
 <template>
   <footer class="footer">
-    <div class="bg-gray-950 text-stone-50 pt-20 pb-16 md:py-20">
+    <div class="pt-12 pb-16 md:py-12 border-t border-stone-200">
       <div class="lg:flex justify-between items-center md:flex-row-reverse layout-full">
-        <div v-if="socialMedia?.socialLinks?.length" class="flex justify-center lg:justify-normal lg:mr-0 mb-12 lg:mb-0 gap-4">
+        <div v-if="socialMedia?.socialLinks?.length" class="flex justify-center lg:justify-normal lg:mr-0 mb-8 lg:mb-0 gap-4">
           <a
             v-for="link in socialMedia.socialLinks"
             :key="link._key"
             :href="link.url"
             target="_blank"
             rel="noopener noreferrer"
-            class="text-stone-200 hover:text-brand-red transition-colors"
-          >
-            {{ link.name }}
-          </a>
+            class="hover:text-brand-red transition-colors font-medium"
+          >{{ link.name }}</a>
         </div>
         <div class="text-center lg:text-left">
-          <h1 class="text-2xl text-stone-50 font-heading mb-2">
+          <h1 class="text-2xl font-heading mb-1">
             <NuxtLink to="/" class="hover:text-brand-red">{{ siteTitle }}</NuxtLink>
           </h1>
-          <p class="text-stone-200 text-sm max-w-60 lg:max-w-full mx-auto">
+          <p class="text-sm max-w-52 lg:max-w-full mx-auto">
             {{ footer?.tagline || 'Default tagline' }}
           </p>
         </div>
